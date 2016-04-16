1b9b94ef42648057f2439ad1a9f856e4b5941d42
Adrian O'Grady <akogrady@hotmail.com>
2016-04-16 11:29:59 +0100

    Cleaned up the code for SelectionSort.

--- Algorithms/src/SelectionSort.cs
+++ Algorithms/src/SelectionSort.cs
@@ -7,17 +7,12 @@
             for (var i = 0; i < array.Length - 1; i++)
             {
                 var smallestIndex = i;
-                var smallestValue = array[i];
 
                 for (var j = i + 1; j < array.Length; j++)
-                {
-                    if (array[j] < smallestValue)
-                    {
+                    if (array[j] < array[smallestIndex])
                         smallestIndex = j;
-                        smallestValue = array[j];
-                    }
-                }
 
+                var smallestValue = array[smallestIndex];
                 array[smallestIndex] = array[i];
                 array[i] = smallestValue;
             }
