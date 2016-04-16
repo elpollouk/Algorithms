5d1cf5905bc4ac3d87a61c7c3b69314c670275ea
Adrian O'Grady <akogrady@hotmail.com>
2016-04-16 11:12:28 +0100

    Updated to NUnit 3 (requires manually installing test adapter) Provided implementation for selection sort

--- Algorithms/src/SelectionSort.cs
+++ Algorithms/src/SelectionSort.cs
@@ -10,7 +10,23 @@ namespace Algorithms
     {
         public void Sort(int[] array)
         {
+            for (var i = 0; i < array.Length - 1; i++)
+            {
+                var smallestIndex = i;
+                var smallestValue = array[i];
 
+                for (var j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < smallestValue)
+                    {
+                        smallestIndex = j;
+                        smallestValue = array[j];
+                    }
+                }
+
+                array[smallestIndex] = array[i];
+                array[i] = smallestValue;
+            }
         }
     }
 }
